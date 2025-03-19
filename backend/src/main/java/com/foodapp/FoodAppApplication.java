0ffc78a811ccdc95d8bdfd6ada1a7cd4eef98190
ssentrela <148038410+ssentrela@users.noreply.github.com>
2025-03-19 18:33:02 +0700

    Update FoodAppApplication.java

--- backend/src/main/java/com/foodapp/FoodAppApplication.java
+++ backend/src/main/java/com/foodapp/FoodAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FoodAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FoodAppApplication.class, args);
+		SpringApplication.run(args);
 	}
 
 }
